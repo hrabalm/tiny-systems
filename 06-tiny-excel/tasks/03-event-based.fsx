bf81f8a612ee89804aa5f7b77ef637273c97c16b
Miroslav Hrabal <mhn@email.cz>
2024-01-23 23:33:10 +0100

    wip: excel-03

--- 06-tiny-excel/tasks/03-event-based.fsx
+++ 06-tiny-excel/tasks/03-event-based.fsx
@@ -4,22 +4,22 @@
 
 type Address = int * int
 
-type Value = 
-  | Number of int
-  | String of string
-  | Error of string
-  
-type Expr = 
-  | Const of Value
-  | Reference of Address
-  | Function of string * Expr list
+type Value =
+    | Number of int
+    | String of string
+    | Error of string
+
+type Expr =
+    | Const of Value
+    | Reference of Address
+    | Function of string * Expr list
 
 // Node in a dependency graph that represents a spreadsheet cell
 // For each cell, we store the original expression, evalauted value
 // and an event to be triggered when the value changes.
-type CellNode = 
-  { mutable Value : Value
-    mutable Expr : Expr } 
+type CellNode =
+    { mutable Value: Value
+      mutable Expr: Expr }
 
 // A live spreadsheet is a mapping from addresses to graph nodes
 type LiveSheet = Map<Address, CellNode>
@@ -28,56 +28,73 @@ type LiveSheet = Map<Address, CellNode>
 // Reactive evaluation and graph construction
 // ----------------------------------------------------------------------------
 
-let rec eval (sheet:LiveSheet) expr = 
-  // TODO: Modify the 'Reference' case. Instead of recursively calling 
-  // 'eval', this should now locate the graph node and return the 'Value'
-  // that is stored in the graph node!
-  failwith "implemented in step 1"
-  
+let rec eval (sheet: LiveSheet) expr =
+    // Modify the 'Reference' case. Instead of recursively calling
+    // 'eval', this should now locate the graph node and return the 'Value'
+    // that is stored in the graph node!
+    match expr with
+    | Const x -> x
+    | Reference x ->
+        match Map.tryFind x sheet with
+        | Some y -> y.Value
+        | _ -> Error "Missing value"
+    | Function(f, list) ->
+        match (f, list) with
+        | ("+", [ x; y ]) ->
+            match ((eval sheet x), (eval sheet y)) with
+            | (Number xn, Number xy) -> Number(xn + xy)
+            | _ -> Error "Invalid arguments"
+        | ("*", [ x; y ]) ->
+            match ((eval sheet x), (eval sheet y)) with
+            | (Number xn, Number xy) -> Number(xn * xy)
+            | _ -> Error "Invalid arguments"
+        | _ -> Error "Unknown"
 
-let makeNode addr (sheet:LiveSheet) (expr:Expr) : CellNode = 
-  // TODO: Create a dependency graph node. In this step, we just want
-  // to get the same functionality as before (i.e., no event handling)
-  // so evaluate the expression, store it and return the node.
-  failwith "not implemented"
 
 
-let makeSheet (list:(Address * Expr) list) : LiveSheet = 
-  // TODO: Previously, we could turn a list of mappings into a sheet just
-  // by using Map.ofList. This no longer works, because we need to add
-  // cells one by one (we should make sure that all cells on which the new one
-  // depends are already in the sheet, but we assume examples are given
-  // in a correct order). To do this, use 'List.fold' and 'makeNode'. 
-  failwith "not implemented"
+let makeNode addr (sheet: LiveSheet) (expr: Expr) : CellNode =
+    // Create a dependency graph node. In this step, we just want
+    // to get the same functionality as before (i.e., no event handling)
+    // so evaluate the expression, store it and return the node.
+    let value = eval sheet expr
+    { Expr = expr; Value = value }
+
+
+let makeSheet (list: (Address * Expr) list) : LiveSheet =
+    // TODO: Previously, we could turn a list of mappings into a sheet just
+    // by using Map.ofList. This no longer works, because we need to add
+    // cells one by one (we should make sure that all cells on which the new one
+    // depends are already in the sheet, but we assume examples are given
+    // in a correct order). To do this, use 'List.fold' and 'makeNode'.
+    let sheet = Map.empty
+    List.fold (fun sheet (addr, expr) -> makeNode sheet expr) sheet list
 
 
 // ----------------------------------------------------------------------------
 // Drag down expansion
 // ----------------------------------------------------------------------------
 
-let rec relocateReferences (srcCol, srcRow) (tgtCol, tgtRow) (srcExpr:Expr) = 
-  failwith "implemented in step 2"
+let rec relocateReferences (srcCol, srcRow) (tgtCol, tgtRow) (srcExpr: Expr) = failwith "implemented in step 2"
 
 
-let expand (r1, c1) (r2, c2) (sheet:LiveSheet) = 
-  // TODO: This needs to call 'makeNode' and add the resulting node, 
-  // instead of just adding the expression to the map as is.
-  failwith "implemented in step 2"
+let expand (r1, c1) (r2, c2) (sheet: LiveSheet) =
+    // TODO: This needs to call 'makeNode' and add the resulting node,
+    // instead of just adding the expression to the map as is.
+    failwith "implemented in step 2"
 
 
 // ----------------------------------------------------------------------------
 // Helpers and test cases
 // ----------------------------------------------------------------------------
 
-let addr (s:string) = 
-  failwith "implemented in step 1"
+let addr (s: string) = failwith "implemented in step 1"
 
-let fib =  
-  [ addr "A1", Const(Number 0) 
-    addr "A2", Const(Number 1)
-    addr "A3", Function("+", [Reference(addr "A1"); Reference(addr "A2")]) ]
-  |> makeSheet
-  |> expand (addr "A3") (addr "A10")
+let fib =
+    [ addr "A1", Const(Number 0)
+      addr "A2", Const(Number 1)
+      addr "A3", Function("+", [ Reference(addr "A1"); Reference(addr "A2") ]) ]
+    |> makeSheet
+    |> expand (addr "A3") (addr "A10")
 
 // Should return: Number 13
 eval fib (Reference(addr "A8"))
@@ -89,14 +106,14 @@ eval fib (Reference(addr "A10"))
 eval fib (Reference(addr "A11"))
 
 
-let fac = 
-  [ addr "A2", Const(Number 1)
-    addr "A3", Function("+", [Reference(addr "A2"); Const(Number 1)])
-    addr "B1", Const(Number 1)
-    addr "B2", Function("*", [Reference(addr "A2"); Reference(addr "B1")]) ] 
-  |> makeSheet
-  |> expand (addr "A3") (addr "A11")
-  |> expand (addr "B2") (addr "B11")
+let fac =
+    [ addr "A2", Const(Number 1)
+      addr "A3", Function("+", [ Reference(addr "A2"); Const(Number 1) ])
+      addr "B1", Const(Number 1)
+      addr "B2", Function("*", [ Reference(addr "A2"); Reference(addr "B1") ]) ]
+    |> makeSheet
+    |> expand (addr "A3") (addr "A11")
+    |> expand (addr "B2") (addr "B11")
 
 // Should return: Number 5
 eval fac (Reference(addr "A6"))
